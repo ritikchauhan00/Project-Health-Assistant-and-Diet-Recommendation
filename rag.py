9c4f14d403c864521c424838c1fd30f84afd9097
Ritik Chauhan <ritikchauhan8859276@gmail.com>
2026-09-24 15:58:39 +0530

    Refactor Create_Rag and load_rag to use HF_TOKEN
    Added HF_TOKEN environment variable for HuggingFace model initialization.

--- rag.py
+++ rag.py
@@ -1,25 +1,34 @@
-import os 
+import os
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
-PDF_File="data/nutrition.pdf"
+
+PDF_File = "data/nutrition.pdf"
+HF_TOKEN = os.getenv("HF_TOKEN")
 
 def Create_Rag():
     if not os.path.exists(PDF_File):
-        raise FileExistsError("File Not Found")
+        raise FileNotFoundError("File Not Found")
     else:
-        document=PyPDFLoader(PDF_File).load()
-        splitter=RecursiveCharacterTextSplitter(chunk_size=500,chunk_overlap=50)
-        chunks=splitter.split_documents(document)
-        embedding=HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-        db=FAISS.from_documents(chunks,embedding)
+        document = PyPDFLoader(PDF_File).load()
+        splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+        chunks = splitter.split_documents(document)
+        
+        embedding = HuggingFaceEmbeddings(
+            model_name="sentence-transformers/all-MiniLM-L6-v2",
+            model_kwargs={'token': HF_TOKEN}
+        )
+        
+        db = FAISS.from_documents(chunks, embedding)
         db.save_local("vector_db")
         return len(chunks)
 
-
 def load_rag():
-    embedding=HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-    db=FAISS.load_local("vector_db",embedding,allow_dangerous_deserialization=True)
-    return db   
+    embedding = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2",
+        model_kwargs={'token': HF_TOKEN}
+    )
+    db = FAISS.load_local("vector_db", embedding, allow_dangerous_deserialization=True)
+    return db
 
